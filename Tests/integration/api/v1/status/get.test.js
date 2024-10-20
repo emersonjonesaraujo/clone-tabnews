7e05c41b27b69ad319072a6d317a25a539458879
Emerson <77971129+emersonjonesaraujo@users.noreply.github.com>
2024-10-20 21:45:21 +0000

    Finaliza o endpoint de status

--- Tests/integration/api/v1/status/get.test.js
+++ Tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,17 @@
+//Sempre que o run npm test:watch é executado este JS será executado
 test("GET to /api/v1/status should return 200", async () => {
+  //teste para identificar se conexão da pasta status está funcionando
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.dependencies.database.version_pg).toEqual("16.4");
+  expect(responseBody.dependencies.database.max_conections).toBeGreaterThan(0);
+  expect(responseBody.dependencies.database.conections_used).toEqual(1);
+
+  //teste para identificar se a data atual é válida e é no minimo igual a data inicial
+  //do timestemp 01.01.1970
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  console.log(responseBody);
 });
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,59 @@
+//pagina responsávle por mostrar os dados sobre o status da aplicação
+
+//busca os dados de acesso ao banco
 import database from "infra/database.js";
+//busca o json do package.json
+import pkg from "package.json";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valorização" });
+  //busca a data corrente no formato ISO
+  const updatedAt = new Date().toISOString();
+
+  //busca o json do package.json
+  //const pkgVersionPg = pkg.dependencies.pg. ";" identifica ao banco o fim da query;
+  let text = "SHOW server_version;";
+  //  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionResult = await database.query(text);
+
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  //select no postgree para identificar o max de conexões permitidas
+  //text = "SELECT current_setting('max_connections')";
+  text = "SHOW max_connections;";
+  const resultMax = await database.query(
+    text,
+    //  "SELECT current_setting('max_connections')",
+  );
+  //acessa o array 0 de resultMax.row para obter o valor de current_setting
+
+  //let numberString = resultMax.rows[0].current_setting;
+  let numberString = resultMax.rows[0].max_connections;
+  const dbMaxConections = parseInt(numberString);
+
+  //select no postgree para identificar o numero de conexões ativas
+  //text = "SELECT * FROM pg_stat_activity WHERE state = 'active'";
+  //:: é um type casting convertendo o count para int
+  //'$(dnname)' parametro dinamico
+  const dbname = process.env.POSTGRES_DB; //"local_db";
+  const resultac = await database.query({
+    text: "select count(*)::int  from pg_stat_activity WHERE  datname = $1;",
+    values: [dbname],
+    //    "SELECT * FROM pg_stat_activity WHERE state = 'active'",
+  });
+  const dbOpenConections = resultac.rows[0].count;
+  console.log(dbOpenConections);
+  //let conectionsUsed = parseInt(numberString);
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version_pg: databaseVersionValue,
+        max_conections: dbMaxConections,
+        conections_used: dbOpenConections,
+      },
+    },
+  });
 }
 
 export default status;
