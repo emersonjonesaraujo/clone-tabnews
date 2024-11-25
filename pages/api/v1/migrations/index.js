fc1697df43ac6d51feb99742f7de05383dce0f48
Emerson <77971129+emersonjonesaraujo@users.noreply.github.com>
2024-11-25 23:50:03 +0000

    correção do metodo migrations index.js para não poermitir metodos invalidos

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,11 +1,9 @@
-import migrationsRunner, { Migration } from "node-pg-migrate";
+import migrationRunner from "node-pg-migrate";
 //possibilita a concatenacao das pastas sem usar / ou \
-import { join } from "node:path";
-import database from "infra/database.js";
+import { resolve } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(request, response) {
-  //const dbClient = await database.getNewClient();
-
   const allowedMethods = ["GET", "POST"];
 
   if (!allowedMethods.includes(request.method)) {
@@ -18,33 +16,31 @@ export default async function migrations(request, response) {
   try {
     dbClient = await database.getNewClient();
 
-    const defaultMigrationsOptions = {
-      //Eexecuta a migration sem atulizar o banco
-      dbClient: dbClient,
+    const defaultMigrationOptions = {
+      dbClient,
+      databaseUrl: process.env.DATABASE_URL,
       dryRun: true,
-      dir: join("infra", "migrations"),
+      dir: resolve("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "migrationsTable",
     };
 
     if (request.method === "GET") {
-      const pendingMigrations = await migrationsRunner(
-        defaultMigrationsOptions,
-      );
-
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
       return response.status(200).json(pendingMigrations);
     }
 
     if (request.method === "POST") {
-      const migratedMigrations = await migrationsRunner({
-        ...defaultMigrationsOptions,
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
         dryRun: false,
       });
 
       if (migratedMigrations.length > 0) {
         return response.status(201).json(migratedMigrations);
       }
+
       return response.status(200).json(migratedMigrations);
     }
   } catch (error) {
