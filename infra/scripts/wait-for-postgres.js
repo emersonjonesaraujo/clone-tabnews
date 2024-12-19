f98eb8e7d741bda51c9c68ed65d5740b4cde46e4
Emerson <77971129+emersonjonesaraujo@users.noreply.github.com>
2024-12-19 19:30:19 +0000

    Adicionado  `wait-for-postgres` no `/infra/scripts`

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,18 @@
+//importar o modulo. o require não transpila - Aula 28 pista lenta 1
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec("docker exec postgre-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+    console.log("\n🟢 Postgress está aceitando conexões!\n");
+  }
+}
+
+process.stdout.write("\n\n🔴 Aguardando postgres");
+checkPostgres();
